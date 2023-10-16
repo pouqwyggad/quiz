925e4ebf9349eed073919708d9d8d9c62d2369ba
pouqwyggad <sultanusmanov176@gmail.com>
2023-10-17 02:40:42 +0300

    added a static pack page, with mock data

---
+++ src/components/ui/FriendsPackList/dataPack.ts
@@ -0,0 +1,45 @@
+export const dataPack = [
+  {
+    Question: 'How "This" works in JavaScript?',
+    Answer: 'This is how "This" works in JavaScript',
+    LastUpdated: '18.03.2021',
+    grade: true,
+  },
+  {
+    Question: 'How "This" works in JavaScript?',
+    Answer: 'This is how "This" works in JavaScript',
+    LastUpdated: '12.03.2021',
+    grade: true,
+  },
+  {
+    Question: 'How "This" works in JavaScript?',
+    Answer: 'This is how "This" works in JavaScript',
+    LastUpdated: '34.03.3233',
+    grade: true,
+  },
+  {
+    Question: 'How "This" works in JavaScript?',
+    Answer: 'This is how "This" works in JavaScript',
+    LastUpdated: '18.03.3434',
+    grade: true,
+  },
+  {
+    Question: 'How "This" works in JavaScript?',
+    Answer: 'This is how "This" works in JavaScript',
+    LastUpdated: '43.03.2021',
+    grade: true,
+  },
+  {
+    Question: 'How "This" works in JavaScript?',
+    Answer: 'This is how "This" works in JavaScript',
+    LastUpdated: '65.03.2021',
+    grade: true,
+  },
+];
+
+export interface IDataPack {
+  Question: string
+  Answer: string
+  LastUpdated: string
+  grade: boolean
+}
--- src/components/ui/LayoutList/LayoutList.tsx
+++ src/components/ui/LayoutList/LayoutList.tsx
@@ -1,34 +1,51 @@
 import React, { FC, PropsWithChildren } from 'react';
+import { useNavigate } from '@tanstack/react-router';
 import classes from './LayoutList.module.scss';
 import { DataType } from '../../pages/Main/dataType';
+import { HatIcon } from '../../icons/HatIcon';
+import { EditIcon } from '../../icons/EditIcon';
+import { TrashCanIcon } from '../../icons/TrashCanIcon';
 
 interface LayoutListProps {
   data: DataType[]
 }
 
-export const LayoutList: FC<PropsWithChildren<LayoutListProps>> = ({ data }) => (
-  <div className={classes.Container}>
-    <table className={classes.Table}>
-      <tr className={classes.TitleRow}>
-        <th>Name</th>
-        <th>Cards</th>
-        <th>LastUpdated</th>
-        <th>CreatedBy</th>
-        <th>Actions</th>
-      </tr>
-      {data.map((item) => (
-        <tr
-          key={item.Name}
-          className={classes.DefaultRow}
-        >
-          <td>{item.Name}</td>
-          <td>{item.Cards}</td>
-          <td>{item.LastUpdated}</td>
-          <td>{item.CreatedBy}</td>
-          <td>{item.Actions}</td>
-        </tr>
-      ))}
-    </table>
-  </div>
+export const LayoutList: FC<PropsWithChildren<LayoutListProps>> = ({ data }) => {
+  const navigate = useNavigate({ from: '/' });
+  const handleClick = () => {
+    navigate({ to: '/pack' });
+  };
 
-);
+  return (
+    <div className={classes.Container}>
+      <table className={classes.Table}>
+        <tr className={classes.TitleRow}>
+          <th>Name</th>
+          <th>Cards</th>
+          <th>LastUpdated</th>
+          <th>CreatedBy</th>
+          <th>Actions</th>
+        </tr>
+        {data.map((item) => (
+          <tr
+            key={item.Name}
+            className={classes.DefaultRow}
+          >
+            <td>{item.Name}</td>
+            <td>{item.Cards}</td>
+            <td>{item.LastUpdated}</td>
+            <td>{item.CreatedBy}</td>
+            <td>
+              <div className={classes.ActionsContainer}>
+                <HatIcon onClick={handleClick} />
+                <EditIcon onClick={handleClick} width="24" height="24" />
+                <TrashCanIcon />
+              </div>
+            </td>
+          </tr>
+        ))}
+      </table>
+    </div>
+
+  );
+};
